0c7d6e60be5bf4695b9002bb61864ee53fb9139c
mashallaryan <mashallaryan@ecs.vuw.ac.nz>
2017-05-11 14:42:37 +1200

    add weightdecay to droupout net

--- examples/optimization_droupout_ex.py
+++ examples/optimization_droupout_ex.py
@@ -13,9 +13,9 @@ def g(x):
     return np.sum(x*np.sin(x))
 
 # bounding box
-box = np.array([[-3],[3]]) 
+box = np.array([[-3],[3]])
 # number of training points
-n_training = 5
+n_training = 2
 # dimensionality
 dim = 1
 #bounding box limits for all variables
@@ -41,11 +41,11 @@ model_type = MlpRegressor
 
 #parameter configurations:
 dropout_types = ['zero_one','Gaussian']
-dropout_rates =[0.5, 0.7]
+dropout_rates =[0.1, 0.05]
 #hiddenlayerCount = [ 5, 7]
 
 transfer_funcs = ['relu','tanh']
-structures = [ (1,600,1),(1,200,200,200,1),(1,300,300,1),(1,250,100,250,1),(1,295,10,295,1),(1,100,100,100,100,100,100,1)]
+structures = [ (1,200,200,200,1),(1,300,300,1),(1,250,100,250,1),(1,295,10,295,1),(1,100,100,100,100,100,100,1)][::-1]
 j=0
 for dropout_type in dropout_types:
     for dropout_rate in dropout_rates:
@@ -54,32 +54,33 @@ for dropout_type in dropout_types:
                 # for the description of parameters goto "feed_forward_net.py" and optimization.py
                 bbopt = Optimizer( bounding_box=box, init_observatins=[xt,yt]
                                     , evaluation_function = g
-                                    , cma_popsize=20, cma_max_itr=1, model_type=MlpRegressor
-                                    , structure=structure 
+                                    , cma_popsize=20, cma_max_itr=20, model_type=MlpRegressor
+                                    , structure=structure
                                     , dropout_rate=dropout_rate
                                     , dropout_type=dropout_type #{ "zero_one" or "Gaussian"}
                                     , use_dropout_ontest = True
                                     , use_batch_normalization=True
                                     , use_early_stopping=True
                                     , transfer_func=transfer_func
-                                    , num_training_steps=2
-                                    , learning_rate=0.1
+                                    , num_training_steps=500000
+                                    , learning_rate=1.0e-1
                                     , ensemble_size=30
-                                    , batch_size=30
+                                    , batch_size=32
+                                    , weight_decay=1.0e-6
                                     , standardize=True)
                 fig, axes = plt.subplots(3, 4)
                 for ax in np.array(axes).flatten():
                       next_point,next_pointy = bbopt.step()
-                      mean, std,_ = bbopt.model.predict(x)    
+                      mean, std,_ = bbopt.model.predict(x)
                       plot_1d(ax,x,y,bbopt.observations[0], bbopt.observations[1], mean, std)
                       ax.plot(bbopt.best['X'],bbopt.best['Y'],'go')
                       ax.plot(next_point,next_pointy,'ro')
-                      ax.set_ylim([-12,12])                      
+                      ax.set_ylim([-12,12])
                       fig.savefig(dropout_type+'_'+str(dropout_rate)+'_'+transfer_func+'_'+str(i)+'.svg')
-                      fig, ax = plt.subplots(1, 1)
-                      ax.plot(mlp.history.losses)
+                      fig1, ax1 = plt.subplots(1, 1)
+                      ax1.plot(bbopt.model.history.losses)
                       j+=1
-                      fig.savefig('error_{}.svg'.format(j))
+                      fig1.savefig('error_{}.svg'.format(j))
 
 #if __name__ == '__main__':
 plt.show(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
--- models/feed_forward_net.py
+++ models/feed_forward_net.py
@@ -4,44 +4,46 @@ from sklearn import preprocessing as pre
 import keras
 from keras.layers.core import Lambda
 from keras import backend as K
+from keras.initializers import *
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, BatchNormalization, Dense
 from keras.layers.noise import GaussianDropout
 import numpy as np
 from mashall.utils import *
 class MlpRegressor(object):
-     
+
 
     def __init__(self,**kwargs):
         """
-        
+
         Args:
-        
+
             'structure': a tuple representing the architecture of the neural net. e.g (3,4,4,1) is a ann with 3 input
                      nodes and 2 hidden layers each containing 4 units and a single output
             'transfer_funcs': activation function(s) of the hidden layers: can be tuple if each layer has
                      a different activation function type
-            'learning_rate' : learning rate for the optimization algorithm  
+            'learning_rate' : learning rate for the optimization algorithm
             'dropout_rate': float (0,1)
                             dropout rate (1- keep_prob) used for dropout while training
             'dropout_type': { "zero_one" or "Gaussian"}
-                            type of dropout used 
+                            type of dropout used
             'use_dropout_ontest' : booelan
                                     A flag indicating the presence of dropout during the prediction/test
             'num_training_steps':  inr
-                            number of training steps     
-            'ensemble_size': int 
+                            number of training steps
+            'ensemble_size': int
                             using dropout during testing/prediction  prediction is performed using an ensemble of ANNs  with its size set to ensemble_size
             'batch_size':   int
-                        batch size if the training set is large    
+                        batch size if the training set is large
             'standardize': boolean (default=True)
                           a flag to indicate if the feature vector should be standardized: (zero mean unit variance)
             'use_batch_normalization': boolean (default=True)
                                         indicates if batch normalization is used
-            'use_early_stopping': Boolean (default=True)                            
+            'use_early_stopping': Boolean (default=True)
                                     Will stop the training after a specific number of no improvement in 'loss' value
                                     (curently parameters are hard coded)
-            'use_batch_normalization':       Boolean (default=True)                    
+            'use_batch_normalization':       Boolean (default=True)
+            'weight_decay' : real (default=None)
         """
         #initialization of the properties
         self.structure = kwargs.get('structure',None)
@@ -50,12 +52,13 @@ class MlpRegressor(object):
         self.dropout_rate = kwargs.get('dropout_rate', 0.3 )
         self.dropout_type = kwargs.get('dropout_type', 'zero_one')
         self.use_dropout_ontest = kwargs.get('use_dropout_ontest', True )
-        self.num_training_steps = kwargs.get('num_training_steps', 1000)            
-        self.ensemble_size = kwargs.get('ensemble_size', 50)            
+        self.num_training_steps = kwargs.get('num_training_steps', 1000)
+        self.ensemble_size = kwargs.get('ensemble_size', 50)
         self.batch_size = kwargs.get('batch_size', 1)
         self.standardize = kwargs.get('standardize', True)
         self.use_early_stopping = kwargs.get('use_early_stopping', True)
         self.use_batch_normalization =  kwargs.get('use_batch_normalization', True)
+        self.weight_decay = kwargs.get('weight_decay', None)
         self.scaler = None
         # self.X, self.Y, self.regularization_factor, self.keep_prop = None, None, None, None
         # self.prediction, self.loss, self.step = None, None, None
@@ -65,38 +68,43 @@ class MlpRegressor(object):
     def build_model(self):
         """
         builds the structure of the Neural net
-                                
+
         """
         #sequentioal model
         self.model = Sequential()
-    
-        #first layer 
+        initseed = 1234
+        #first layer
 
-        self.model.add(Dense(self.structure[1],kernel_initializer='normal',input_dim=self.structure[0]))
+        kernel_initializer = {'relu':he_normal,'tanh':glorot_uniform}[self.transfer_func]
+        kernel_regularizers = [None]*self.n_layers
+        if self.weight_decay:
+            kernel_regularizers=[keras.regularizers.l2(self.weight_decay) for _ in  range(self.n_layers)]
+        self.model.add(Dense(self.structure[1],kernel_initializer=kernel_initializer(seed=initseed),kernel_regularizer=kernel_regularizers[0],input_dim=self.structure[0]))
         for layer in range(2,self.n_layers):
-            # add  batch normalization 
+            # add  batch normalization
             if self.use_batch_normalization:
                 self.model.add(BatchNormalization())
-            # activation function    
+            # activation function
             self.model.add(Activation(self.transfer_func))
-            # drop out 
+            # drop out
             self.model.add({'zero_one':{False:Dropout(self.dropout_rate),# self.use_dropout_ontest == False
                              True:Lambda(lambda x: K.dropout(x, level=self.dropout_rate))},#self.use_dropout_ontest == True
                              'Gaussian':{False:GaussianDropout(self.dropout_rate),# self.use_dropout_ontest == False
                              True:Lambda(lambda x:   x*K.random_normal(shape=K.shape(x),
                                                 mean=1.0,
                                                 stddev=np.sqrt(self.dropout_rate / (1.0 - self.dropout_rate))))}}[self.dropout_type][ self.use_dropout_ontest])
-                                                
-            # next dense layer    
-            self.model.add(Dense(self.structure[layer],kernel_initializer='normal'))
+
+            # next dense layer
+            initseed+=1000
+            self.model.add(Dense(self.structure[layer],kernel_initializer=kernel_initializer(seed=initseed), kernel_regularizer=kernel_regularizers[layer-1]))
         optimizer = keras.optimizers.Adam(lr=self.learning_rate)    #lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0
 
     	self.model.compile(loss='mean_squared_error', optimizer=optimizer)
-        
-    
+
+
     def fit(self, xt, yt):
         """
-        fit the model to the training set 
+        fit the model to the training set
         """
         optimizer = keras.optimizers.Adam(lr=self.learning_rate)    #defaut: lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0
     	self.model.compile(loss='mean_squared_error', optimizer=optimizer)
@@ -107,24 +115,24 @@ class MlpRegressor(object):
             x = self.scaler.transform(xt)
         y = np.atleast_2d(yt)
         assert xt.shape[0] == yt.shape[0]
-        callbacks = [keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.8, patience=50, min_lr=1.0e-5)]
+        callbacks = [keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=2000, min_lr=1.0e-5)]
         self.history = LossHistory()
         callbacks += [self.history]
         if self.use_early_stopping:
-            callbacks+=[keras.callbacks.EarlyStopping(monitor='loss', min_delta=1.0e-5, patience=5000, verbose=0, mode='auto')]
+            callbacks+=[keras.callbacks.EarlyStopping(monitor='loss', min_delta=1.0e-5, patience=28000, verbose=0, mode='auto')]
         self.model.fit(x,yt, batch_size= self.batch_size, epochs=self.num_training_steps, callbacks=callbacks)
 #        scores = self.model.evaluate(xt, yt)
 
-        
+
 
     def single_prediction(self, x):
         if self.standardize:
             return self.model.predict(self.scaler.transform(x))
-        else:    
+        else:
             return self.model.predict(x)
 
     def predict(self, xt):
-        
+
         xs =  np.atleast_2d(xt)
         ys = []
         for _ in range(self.ensemble_size):
@@ -142,12 +150,12 @@ class MlpRegressor(object):
     @property
     def n_layers(self):
         return len(self.structure)
-            
+
 
     @property
     def structure(self):
         return self._structure
-    
+
     @structure.setter
     def structure(self, value):
         if not (value or isinstance(value, tuple)):
@@ -160,10 +168,10 @@ class MlpRegressor(object):
                     raise ValueError('structure must contain non zero integer items.')
         self._structure =  value
 
-    @property  
+    @property
     def transfer_func(self):
         return self._transfer_func
-    
+
     @transfer_func.setter
     def transfer_func(self, value):
         self._transfer_func = value
@@ -171,14 +179,14 @@ class MlpRegressor(object):
     @property
     def learning_rate(self):
         return self._learning_rate
-    @learning_rate.setter            
+    @learning_rate.setter
     def learning_rate(self, value):
         self._learning_rate = value
-    
+
     @property
     def dropout_rate(self):
         return self._dropout_rate
-    
+
     @dropout_rate.setter
     def dropout_rate(self, value):
         self._dropout_rate = value
@@ -186,14 +194,14 @@ class MlpRegressor(object):
     @property
     def use_dropout_ontest(self):
         return self._use_dropout_ontest
-    
+
     @use_dropout_ontest.setter
     def use_dropout_ontest(self, value):
         self._use_dropout_ontest = value
 
 
 
-    @property     
+    @property
     def num_training_steps(self):
         return self._num_training_steps
     @num_training_steps.setter
@@ -201,10 +209,10 @@ class MlpRegressor(object):
         self._num_training_steps = value
 
 
-    @property     
+    @property
     def ensemble_size(self):
         return self._ensemble_size
-    
+
     @ensemble_size.setter
     def ensemble_size(self, value):
         self._ensemble_size = value
